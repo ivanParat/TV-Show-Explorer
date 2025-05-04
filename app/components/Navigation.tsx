df282fdebfe33f5856d274584f537873b5989ecc
ivanParat <ivan.parat2003@gmail.com>
2025-05-04 21:25:17 +0200

    Fetching shows from API and showing them on home page

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+function SearchBar(){
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [input, setInput] = useState(searchParams.get("q") || "");
+  const handleSearch = () => {
+    router.push(input ? `/?q=${encodeURIComponent(input)}` : "/");
+  };
+
+  useEffect(() => {
+    if (!searchParams.get("q")) {
+      setInput("");
+    }
+  }, [searchParams]);
+
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  return (
+    <>
+      {/* Hidden on mobile */}
+      <div className="hidden md:flex items-center bg-gray-dark rounded-lg px-2 py-1 md:w-32 lg:w-60 xl:w-80">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-full px-2 outline-none text-main-text bg-gray-dark"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+        />
+        <button 
+          className="ml-2 text-main-text hover:text-brand"
+          onClick={handleSearch}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+            <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clipRule="evenodd" />
+          </svg>
+        </button>
+      </div>
+
+      {/* Visible on mobile */}
+      <div className="md:hidden flex justify-center pl-3">
+        {isSearchOpen ? (
+          <div className="flex items-center bg-gray-dark rounded-lg px-2 w-full">
+            <button onClick={() => { setIsSearchOpen(false);}} className="ml-2 text-white hover:text-brand">
+              ✖️
+            </button>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full px-2 outline-none text-main-text bg-gray-dark"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+            />
+            <button className="ml-2  hover:text-brand" onClick={() => { setIsSearchOpen(false); handleSearch()}}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              </svg>
+            </button>
+          </div>
+        ) : (
+          <button className="ml-2  hover:text-brand" onClick={() => { setIsSearchOpen(true);}}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        )}
+      </div>
+    </>
+  )
+}
+
+export function Navigation() {
+
+  return (
+    <nav className="flex h-14 items-center justify-between px-2 md:px-4 lg:px-8 xl:px-12">
+      <div>
+        <SearchBar/>
+      </div>
+      <ul className="flex justify-center items-center">
+        <Link 
+          href={"/favorites"}
+          className="font-bold hover:text-brand active:text-brand px-5 py-1 md:px-1 lg:px-3 xl:px-5"
+        >
+          Favorites
+        </Link>
+
+        <Link
+          href={"/login"}
+          className="font-bold md:ml-1 lg:ml-3 xl:ml-5"
+        >
+          <button className="bg-brand text-white px-6 py-1 rounded-md hover:bg-brand-hover active:bg-brand-hover cursor-pointer">
+            Log in
+          </button>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,33 @@
 import Image from "next/image";
+import Star from "./components/Star";
 
-export default function Home() {
+export default async function Home() {
+  const today = new Date().toISOString().split("T")[0];
+  const res = await fetch(`https://api.tvmaze.com/schedule/web?date=${today}`, { next: { revalidate: 3600 } });
+  const schedule = await res.json();
+
+  //we get shows from the schedule, and crucially - UNIQUE shows, we mustn't have duplicates
+  const showsMap = new Map<number, any>();
+  schedule.forEach((entry: any) => {
+    const show = entry._embedded.show;
+    showsMap.set(show.id, show); // If show.id already exists, it will be overwritten
+  });
+  const shows = Array.from(showsMap.values());
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-6">
+      {shows.map((show: any) => (
+        <div key={show.id} className="bg-gray-800 text-white p-4 rounded">
+          {show.image?.original && (
+            <Image src={show.image.original} alt={show.name} width={210} height={295} />
+          )}
+          <h2 className="text-xl mt-2">{show.name}</h2>
+          <p className="text-sm flex items-end gap-1">
+            {show.rating?.average && <Star/>} 
+            {show.rating?.average ? show.rating.average.toFixed(1) : 'Rating unavailable'}
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
     </div>
   );
 }
