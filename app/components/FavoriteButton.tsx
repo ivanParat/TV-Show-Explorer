c0f0174c353c5f6c7573df3912f8351aa007cc19
ivanParat <ivan.parat2003@gmail.com>
2025-05-10 15:49:44 +0200

    Favorite button's a heart now

--- app/components/FavoriteButton.tsx
+++ app/components/FavoriteButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useTransition, useEffect } from "react";
+import { FaRegHeart, FaHeart } from "react-icons/fa6";
  
 export default function FavoriteButton({ featureId, type, initialSaved }:{featureId:number, type:string, initialSaved?:boolean}) {
   const [saved, setSaved] = useState(initialSaved);   
@@ -29,13 +30,14 @@ export default function FavoriteButton({ featureId, type, initialSaved }:{featur
  
   return (
     <button
-      disabled={isPending}
-      onClick={() => {toggleFavorite(); console.log(featureId); console.log(saved)}}
-      className={`px-3 py-1 rounded text-white transition-colors ${
-        saved ? "bg-green-600" : "bg-amber-500 hover:bg-amber-600"
-      }`}
+    onClick={toggleFavorite}
+    disabled={isPending}
+    className={`text-red-500 cursor-pointer ${
+      isPending ? "opacity-50 cursor-not-allowed" : "hover:text-red-400 active:text-red-400"
+    }`}
+    aria-label={saved ? "Remove from favorites" : "Add to favorites"}
     >
-      {saved ? "Ukloni iz favorita" : isPending ? "Pending..." : "Dodaj u favorite"}
+      {saved ? <FaHeart size={24} /> : <FaRegHeart size={24} />}
     </button>
   );
 }
