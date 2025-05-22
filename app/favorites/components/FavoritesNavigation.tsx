8b1c8ed2285c6f7c8e9225cbfbd1b77300150a9d
ivanParat <ivan.parat2003@gmail.com>
2025-05-22 18:42:11 +0200

    Favorites navigation design

--- app/favorites/components/FavoritesNavigation.tsx
+++ app/favorites/components/FavoritesNavigation.tsx
@@ -1,12 +1,20 @@
+'use client'
+ 
+import { usePathname } from 'next/navigation'
 import Link from "next/link";
 
 const types = ["shows", "episodes", "people"];
 
 export default function FavoritesNavigation(){
+  const pathname = usePathname();
   return (
-    <div className="flex space-x-2">
+    <div className="flex space-x-6 text-lg font-medium justify-center py-3">
       {types.map((type:string, index: number) => (
-        <Link key={index} href={`/favorites/${type}`}>
+        <Link 
+          key={index} 
+          href={`/favorites/${type}`} 
+          className={`${pathname.includes(type) ? "text-gray-300" : "hover:text-gray-300"} active:text-gray-300`}
+        >
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </Link>
       ))}
